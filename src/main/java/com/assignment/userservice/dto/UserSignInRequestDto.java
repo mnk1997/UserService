7786f2e54542262ef6b871977a835e6d7fe4ffe6
mnk1997 <mayankraj2106@gmail.com>
2024-12-03 10:57:47 +0530

    Added passwordEncoder functionality for user sign in

--- src/main/java/com/assignment/userservice/dto/UserSignInRequestDto.java
+++ src/main/java/com/assignment/userservice/dto/UserSignInRequestDto.java
@@ -1,10 +1,42 @@
 package com.assignment.userservice.dto;
 
 
+import com.assignment.userservice.models.Role;
+import com.assignment.userservice.models.User;
 import lombok.Data;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 public class UserSignInRequestDto {
     private String userName;
     private String password;
+    private PasswordEncoder passwordEncoder;
+
+    public UserSignInRequestDto(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+
+    public User toUser(UserSignUpRequestDto signInDto)
+    {
+        User user = new User();
+        user.setEmail(signInDto.getEmail());
+        user.setPassword(passwordEncoder.encode(signInDto.getPassword()));
+        user.setFirstName(signInDto.getFirstName());
+        user.setPhoneNumber(signInDto.getPhoneNumber());
+        List<Role> roles = new ArrayList<>();
+        for(String role : signInDto.getRoles())
+        {
+            Role r=new Role();
+            r.setValue(role);
+           roles.add(r);
+        }
+        user.setRoles(roles);
+        return user;
+    }
+
 }
