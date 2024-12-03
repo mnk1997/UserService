b8a8ee8b0d4446919afcb8aecad85ff7ce39d5c6
mnk1997 <mayankraj2106@gmail.com>
2024-12-03 13:42:46 +0530

    Defined @controllerAdvice and @ExceptionHandler for all the exceptions

--- src/main/java/com/assignment/userservice/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/assignment/userservice/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,57 @@
 package com.assignment.userservice.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(e.getMessage());
+
+    }
+    @ExceptionHandler(IncorrectPhoneNumber.class)
+    public ResponseEntity<String> handleIncorrectPhoneNumber(IncorrectPhoneNumber e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(e.getMessage());
+
+    }
+    @ExceptionHandler(NoRoleSelected.class)
+    public ResponseEntity<String> handleNoRoleSelected(NoRoleSelected e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(e.getMessage());
+
+
+    }
+    @ExceptionHandler(NoValidTokenFoundException.class)
+    public ResponseEntity<String> handleNoValidTokenFoundException(NoValidTokenFoundException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(e.getMessage());
+
+
+    }
+    @ExceptionHandler(PasswordAndUserNameNotMatchedException.class)
+    public ResponseEntity<String> handlePasswordAndUserNameNotMatchedException(PasswordAndUserNameNotMatchedException e) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body(e.getMessage());
+
+
+    }
+    @ExceptionHandler(RoleNotFoundException.class)
+    public ResponseEntity<String> handleRoleNotFoundException(RoleNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(e.getMessage());
+
+
+    }
+    @ExceptionHandler(SessionExpiredException.class)
+    public ResponseEntity<String> handleSessionExpiredException(SessionExpiredException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(e.getMessage());
+
+
+    }
 }
